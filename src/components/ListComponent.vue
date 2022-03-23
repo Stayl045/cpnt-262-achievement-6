8fd7acd47ce256ff1af865c8d1b494186abdc54b
Stephan Taylor <stephantaylor21@hotmail.com>
2022-03-23 17:44:43 -0600

    readme fixes

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <ul>
-    <h3 class="grid place-items-center text-center text-2xl text-blue-400">
+    <h3 class="grid place-items-center text-center text-2xl">
       List of Animals
     </h3>
     <li
       v-for="item in animals"
       :key="item.id"
-      class="place-items-center text-center mx-1 flex flex-col bg-blue-100 rounded-sm py-4"
+      class="place-items-center text-center flex flex-col bg-blue-100 rounded-sm py-4"
     >
       {{ item.animal }}
     </li>
